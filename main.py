1267340c94fc518ae42303a4c4461930afe50fa4
fearfully-m <kmstevensj@gmail.com>
2026-08-06 01:00:24 -0400

    Added check memory utilization feature

--- main.py
+++ main.py
@@ -8,21 +8,29 @@ def main():
     intervalTime = 5 # number of seconds for a measured interval of time
     thresholdPercentage = 90.0 # determined limit considered to be the threshold
     alertDurationSeconds = 10 # duration of when user should be alerted if over threshold 
+    memoryThresholdPercentage = 90 # threshold limit for memory
 
     while True:
         time.sleep(intervalTime)
-        CPU_utilization = psutil.cpu_percent()
+        CPU_utilization = psutil.cpu_percent() # to aborb the first placeholder value
         print(CPU_utilization)
+        memory = psutil.virtual_memory()
+        print(memory.percent)
 
+        # determine if CPU utilization is over threshold 
         if CPU_utilization > thresholdPercentage:
-            timeOverThreshold+=intervalTime
+            timeOverThreshold+=intervalTime # add interval time 
             print("Threshold Percentage:", thresholdPercentage)
-            # print(timeOverThreshold, "this is time overthreshold")
             if timeOverThreshold >= alertDurationSeconds:
                 print("CPU Utilization is over 90 percent.")
 
+        # reset the time over threshold if utilization is under the threshold limit
         elif CPU_utilization <= thresholdPercentage:
             timeOverThreshold = 0
+
+        # check if memory utilization is over the threshold
+        if memory.percent > memoryThresholdPercentage:
+            print("Memory utilization is over 90 percent.")
 
 
 
